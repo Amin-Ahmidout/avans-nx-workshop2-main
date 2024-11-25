05bea674b7d5ae9bed1c6ce766c12ed2fe141567
Amin-Ahmidout <AhmidoutA@hotmail.com>
2024-11-25 15:00:58 +0100

    Update production environment configuration with new ROOT_DOMAIN_URL

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,7 +3,7 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
 
-    ROOT_DOMAIN_URL: 'https://nxworkshop.azurewebsites.net',
+    ROOT_DOMAIN_URL: 'https://ashy-stone-03e313603.5.azurestaticapps.net',
     dataApiUrl: 'https://nxworkshop.azurewebsites.net/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://admin:Admin123!@cluster0.pkmgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
