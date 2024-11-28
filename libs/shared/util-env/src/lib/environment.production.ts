bb3e8bb11126b139db1229a2f607d7e568e1a702
Amin-Ahmidout <AhmidoutA@hotmail.com>
2024-11-28 10:41:48 +0100

    Update Azure Static Web Apps workflow and environment configuration to correct app name and URLs

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,8 +3,8 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
 
-    ROOT_DOMAIN_URL: 'https://books-data-api.azurewebsites.net/',
-    dataApiUrl: 'https://books-data-api.azurewebsites.net/api',
+    ROOT_DOMAIN_URL: 'https://book-data-api.azurewebsites.net/',
+    dataApiUrl: 'https://book-data-api.azurewebsites.net/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://admin:Admin123!@cluster0.pkmgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 };
